8bdc25dcbb59565b3e83ca1dc49ac47bc513486f
Sarah Burnside <sarahburnside787@gmail.com>
2024-02-20 12:51:54 -0500

    Code created after chapter 7

--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,23 @@
 # pylint: disable=no-member,redefine-builtin,
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
 from .models import Notes
 from .forms import NotesForm
 
 # Create your views here.
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name ="notes/notes_delete.html"
+
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
 class NotesCreateView(CreateView):
     model = Notes
     success_url = '/smart/notes'
