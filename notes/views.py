d722711fdbfcb551fa0ac3fe96829727bc0adbe9
Sarah Burnside <sarahburnside787@gmail.com>
2024-02-20 13:10:32 -0500

    Code created after chapter 8

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,11 @@
 # pylint: disable=no-member,redefine-builtin,
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.http import Http404
+from django.http.response import HttpResponseRedirect
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Notes
 from .forms import NotesForm
 
@@ -22,11 +25,22 @@ class NotesCreateView(CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = "/admin"
 
-class NotesListView(ListView):
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name ="notes"
     template_name ="notes/notes_list.html"
+    login_url = "/admin"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model = Notes
