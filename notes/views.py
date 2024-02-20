4ec5a163e39c2ed6038bcfceb1728502cc9ba608
Sarah Burnside <sarahburnside787@gmail.com>
2024-02-20 10:07:33 -0500

    Code created after chapter 4

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,17 @@
+# pylint: disable=no-member,redefine-builtin,
 from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import DetailView, ListView
+from .models import Notes
 
 # Create your views here.
+class NotesListView(ListView):
+    model = Notes
+    context_object_name ="notes"
+    template_name ="notes/notes_list.html"
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name ="note"
+
+
